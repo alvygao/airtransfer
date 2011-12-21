0c81edd996f0f59a1bf3b177367efd82297abb22
sergey.grodzitskiy <sergey.grodzitskiy@26803fc3-51c2-5a0c-1600-d2841f3a60a9>
2011-12-21 19:09:12 +0000

    [sergey] remove check

--- trunk/server/src/main/java/com/airtransfer/rest/services/UserProfileManager.java
+++ trunk/server/src/main/java/com/airtransfer/rest/services/UserProfileManager.java
@@ -38,35 +38,6 @@ public class UserProfileManager extends BaseManager {
             userProfile.setUser(user);
             profileDao.save(userProfile);
         }
-/*
-        userProfile.setAboutMe("aboutMe");
-        userProfile.setAppearance("appearance");
-        userProfile.setBody("body");
-        userProfile.setBooks("books");
-        userProfile.setCellPhone("cellPhone");
-        userProfile.setCity("city");
-        userProfile.setCountry("country");
-        userProfile.setCurrentCity("current city");
-        userProfile.setCurrentCountry("current country");
-        userProfile.setFamilyStatus("family status");
-        userProfile.setFirstLanguage("first language");
-        userProfile.setFistName("first name");
-        userProfile.setHeight(175.40f);
-        userProfile.setInterest("interest");
-        userProfile.setLastName("last name");
-        userProfile.setLifeGoals("goals");
-        userProfile.setMale(true);
-        userProfile.setMovies("movies");
-        userProfile.setMusic("music");
-        userProfile.setOccupation("occupation");
-        userProfile.setPhone("phone");
-        userProfile.setSecondLanguage("secondLanguage");
-        userProfile.setSiteUrl("site url");
-        userProfile.setSkypeId("skype id");
-        userProfile.setThirdLanguage("third language");
-        userProfile.setWidth(75.5f);
-        profileDao.save(userProfile);
-*/
         return new BaseEntityVOResponse<UserProfileVO>(new UserProfileVO(userProfile));
     }
 
@@ -75,13 +46,16 @@ public class UserProfileManager extends BaseManager {
     @Produces({MediaType.APPLICATION_JSON})
     public BaseEntityVOResponse<UserProfileVO> update(BaseEntityVORequest<UserProfileVO> request) {
 
+
         Element data = (Element) request.getData();
 
         UserProfileVO vo = convert(UserProfileVO.class, new UserProfileVO(new UserProfile()), data);
 
         final UserSession session = getSession();
         final User user = session.getUser();
+
         final UserProfile userProfile = profileDao.findProfileByUser(user.getId());
+
         final UserProfile model = vo.model();
         model.setId(userProfile.getId());
         model.setUser(userProfile.getUser());
